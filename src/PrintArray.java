df19469656f6c29b7bd96e2ceca7f759b385761e
KiNG-MBA <kingmba.6663@gmail.com>
2020-12-15 12:26:30 +0330

    Overload PrintArray and add 2 parameter to method!!

--- src/PrintArray.java
+++ src/PrintArray.java
@@ -1,14 +1,36 @@
 public class PrintArray {
     public static void main(String[] args) {
+        String[] text = {"ali", "masoud", "hassan", "mehdi", "saeed"};
+        Integer[] numbers = {12, 432, 523, 81, 15, 9};
+        Double[] decimal = {8.3, 88.34, 50.1, 90.9, 63.33};
+        Character[] characters = {'c', 'd', 't', '5', 'm'};
 
+        printArray(text);
+        printArray(text,0,4);
+        printArray(numbers);
+        printArray(numbers,2,6);
+        printArray(decimal);
+        printArray(characters);
 
     }
 
     public static <G> void printArray(G[] array) {
-        for (G arrays : array ) {
-            System.out.println(arrays);
+        for (G arrays : array) {
+            System.out.print(arrays + " || ");
+        }
+        System.out.println();
+
+    }
+
+    public static <G> void printArray(G[] array, int lowSubscript, int highSubscript) {
+        if (lowSubscript < 0 || highSubscript > array.length-1) {
+            throw new IllegalArgumentException("Out of array range");
         }
 
+        for (int i = lowSubscript; i <highSubscript ; i++) {
+            System.out.print(array[i]+" * ");
+        }
+        System.out.println("#########################");
     }
 
 
