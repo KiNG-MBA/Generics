63934b839cdddf60e496a27c4fe30e3223fe5111
KiNG-MBA <kingmba.6663@gmail.com>
2020-12-15 11:59:38 +0330

    write a generic method to print array!

---
+++ src/PrintArray.java
@@ -0,0 +1,15 @@
+public class PrintArray {
+    public static void main(String[] args) {
+
+
+    }
+
+    public static <G> void printArray(G[] array) {
+        for (G arrays : array ) {
+            System.out.println(arrays);
+        }
+
+    }
+
+
+}
